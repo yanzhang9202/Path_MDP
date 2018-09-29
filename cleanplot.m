7699fcb71036a488a9435c533078a3bb8ad62ef1
Yan Zhang <yan_zhang@Yans-MacBook-Pro.local>
2018-09-29 01:00:26 -0400

    field plot modified

--- cleanplot.m
+++ cleanplot.m
@@ -1,16 +1,29 @@
-axis equal
-axis xy
-ax.XLim = [0,5];
-ax.YLim = [0,5];
 % Setting tick labels
-ax.XTick = 0.5:4.5;
-ax.XTickLabelMode = 'manual';
-ax.XTickLabel = num2cell(string(0:4));
-ax.YTick = 0.5:4.5;
-ax.YTickLabelMode = 'manual';
-ax.YTickLabel = num2cell(string(0:4));
+ax1 = gca;
+ax1.XTick = 0.5:4.5;
+ax1.XTickLabelMode = 'manual';
+ax1.XTickLabel = num2cell(string(0:4));
+ax1.YTick = 0.5:4.5;
+ax1.YTickLabelMode = 'manual';
+ax1.YTickLabel = num2cell(string(0:4));
+ax1.FontSize = 20;
+ax1.LineWidth = 4;
+ax1.DataAspectRatio = [1,1,1];
+% XY limits
+ax1.XLim = [0,5];
+ax1.YLim = [0,5];
 % Setting grid positions
-ax.MinorGridColor = [0,0,0];
-ax.MinorGridAlpha = 1;
-ax.MinorGridLineStyle = '-'
-grid minor
+ax2 = axes('Position', ax1.Position, ...
+    'XAxisLocation', 'bottom', 'YAxisLocation', 'left', ...
+    'Color', 'none', ...
+    'XLim', ax1.XLim, 'YLim', ax1.YLim, ...
+    'XTick', 0:5, 'YTick', 0:5, ...
+    'XTickLabel', [], 'YTickLabel', [], ...
+    'GridAlpha', 1, 'GridColor', [0,0,0], ...
+    'LineWidth', 1.5, ...
+    'DataAspectRatio', [1,1,1]);
+linkaxes([ax1, ax2], 'xy')
+grid on
+set(gcf, 'CurrentAxes', ax1);
+axis xy
+box on;
--- test_plot.m
+++ test_plot.m
@@ -3,16 +3,26 @@ clear;
 close all;
 clc;
 % Define color map
-map = [1,1,1;   % white
-       1,0,0;   % red
-       0,1,0;   % green
-       0,0,1;   % blue
-       1,1,0];  % yellow
+map = [1,1,1;   % white - 1
+       1,0,0;   % red - 2
+       0,1,0;   % green - 3
+       0,0,1;   % blue - 4
+       1,1,0];  % yellow - 5
 
 %% create the field
 C = ones(5);
+C(1,1) = 5;
+C(5,5) = 3;
+C(5,1) = 2;
+C(1,4) = 4;
 figure(101)
+hold on;
+% the field
 image(0.5, 0.5, C);
 colormap(map)
-ax = gca;
+% add walls
+line([1,1],[0,2],'LineWidth',8, 'Color', 'k')
+line([2,2],[3,5],'LineWidth',8, 'Color', 'k')
+line([3,3],[0,2],'LineWidth',8, 'Color', 'k')
+% axes
 cleanplot;
